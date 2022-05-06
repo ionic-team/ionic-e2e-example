71239631896dd30272dd2a79dd821db29ee01511
John Lane <john@ionic.io>
2022-05-06 10:48:36 -0500

    Split clearIndexedDB into 2 separate executes

--- tests/helpers/storage.ts
+++ tests/helpers/storage.ts
@@ -1,12 +1,15 @@
 import { pause } from '.';
 
 export async function clearIndexedDB(dbName: string) {
-  await browser.execute((name) => {
+  await browser.execute((name: string) => {
     indexedDB.deleteDatabase(name);
+  }, dbName);
+
+  await browser.execute(() => {
     // Needed to reload the page for the DB to be reloaded
     // for mobile devices
     document.location.reload();
-  }, dbName);
+  });
 
   return pause(500);
 }
